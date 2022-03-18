730d8739e6817bf0ae47d9fa287b0d35e0919317
John Andrew Moquia <andrewmoquia09@gmail.com>
2022-03-18 16:16:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer  = require('multer'); 
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 var app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.use('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const {originalname, mimetype, size} = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
